d3d91c3d96a88c8d5854349875232f520c19a1bc
Sven Malvik <sven@malvik.de>
2010-11-04 10:21:34 +0100

    Picasa test

--- app/controllers/Picasa.java
+++ app/controllers/Picasa.java
@@ -3,33 +3,33 @@ package controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import models.ContentImage;
 
-import com.google.gdata.client.Query;
 import com.google.gdata.client.photos.PicasawebService;
+import com.google.gdata.data.photos.AlbumFeed;
 import com.google.gdata.data.photos.GphotoEntry;
-import com.google.gdata.data.photos.UserFeed;
 import com.google.gdata.util.ServiceException;
 
 public class Picasa {
 
 	public static List<ContentImage> getImages(String tag) throws IOException, ServiceException {
+		List<ContentImage> images = new ArrayList<ContentImage>();
 		URL metafeedUrl = new URL("https://picasaweb.google.com/data/feed/base/user/svenmalvik/album/cm?imgmax=912&tag=" + tag + "&thumbsize=32&authkey=Gv1sRgCOu93ZCOr6vNWg");
-
 		PicasawebService myService = new PicasawebService("My Application");
-
-        System.out.println("Getting Picasa Web Albums entries...");
-        UserFeed resultFeed = myService.getFeed(metafeedUrl, UserFeed.class);
+        AlbumFeed resultFeed = myService.getFeed(metafeedUrl, AlbumFeed.class);
         List<GphotoEntry> entries = resultFeed.getEntries();
+        
         for(int i=0; i<entries.size(); i++) {
           GphotoEntry entry = entries.get(i);
-          System.out.println("\t" + entry.getTitle().getPlainText());
+          ContentImage image = new ContentImage();
+          image.src = entry.getSelfLink().getHref();
+          images.add(image);
         }
-        System.out.println("Total Entries: "+entries.size());
 		
-		return null;
+		return images;
 	}
 
 }
--- app/models/ContentImage.java
+++ app/models/ContentImage.java
@@ -2,4 +2,6 @@ package models;
 
 public class ContentImage {
 
+	public String src;
+
 }
