b0cdc794bf37c34c46a01c3fce326b7e80c79d7c
Sven Malvik <sven@malvik.de>
2010-11-05 17:08:06 +0100

    Picasa configuration

--- app/controllers/Picasa.java
+++ app/controllers/Picasa.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
+import play.Play;
 
 import models.ContentImage;
 
@@ -16,14 +19,35 @@ import com.google.gdata.util.ServiceException;
 
 public class Picasa {
 
+	private static String AUTHKEY;
+	private static String USER;
+	private static String ALBUM;
+	private static String THUMBSIZES;
+	private static boolean CONFIGS_READ = false;
+	
 	public static List<ContentImage> getImages(String tag) throws IOException, ServiceException {
-		List<ContentImage> images = new ArrayList<ContentImage>();
-		URL metafeedUrl = new URL("https://picasaweb.google.com/data/feed/base/user/svenmalvik/album/cm?imgmax=912&tag=" + tag + "&thumbsize=104&authkey=Gv1sRgCOu93ZCOr6vNWg");
+        readConfigurations();
+		URL metafeedUrl = new URL("https://picasaweb.google.com/data/feed/base/user/" + USER + "/album/" + ALBUM + "?imgmax=912&tag=" + tag + "&thumbsize=" + THUMBSIZES + "&authkey=" + AUTHKEY);
 		PicasawebService myService = new PicasawebService("My Application");
         AlbumFeed resultFeed = myService.getFeed(metafeedUrl, AlbumFeed.class);
-        List<PhotoEntry> entries = resultFeed.getPhotoEntries();
-        
-        for(int i=0; i<entries.size(); i++) {
+        return extractImageList(resultFeed.getPhotoEntries());
+	}
+
+	private static void readConfigurations() {
+		if (!CONFIGS_READ) {
+			Properties p = Play.configuration;
+			USER = p.getProperty("picasa.user", "");
+			ALBUM = p.getProperty("picasa.album", "");
+			AUTHKEY = p.getProperty("picasa.authkey", "");
+			THUMBSIZES = p.getProperty("picasa.thumbsizes", "");
+		}
+		CONFIGS_READ = true;
+	}
+
+	private static List<ContentImage> extractImageList(List<PhotoEntry> entries) {
+		List<ContentImage> images = new ArrayList<ContentImage>();
+		
+        for(int i = 0; i < entries.size(); i++) {
           PhotoEntry photo = entries.get(i);
           ContentImage image = new ContentImage();
           image.src = photo.getMediaContents().get(0).getUrl();
